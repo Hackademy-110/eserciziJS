b1b64388f2cb2389d91639032904b825ba01472e
Vittorio Gatto <gattovitto21@.com>
2023-11-02 16:20:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let age=27;
+let name="Vittorio";
+let subject="java script";
+let teacher="Valerio Vacca";
+let idVik=`ciao sono ${name} ho ${age} anni e sto studiando ${subject} con ${teacher} il miglior docente del globo`;
+
+console.log(idVik);
+
+let number0= 1; 
+let number1= 2;
+let number2= 3;
+let number3= 4;
+let number4= 5;
+
+let somma= number0 + number1 +number2 + number3 + number4;
+let media=somma / 5; 
+
+console.log(`la somma tra i numeri è ${somma} e la loro media è ${media}`);
+
+let annoCorrente= 2023;
+let annoNascita = 1995;
+let età= annoCorrente-annoNascita;
+
+
+console.log(`hai ${età} e ti mancano ${100-età} per compierne 100`);
+
+let totaleGatti= 44;
+let file= 6;
+let restoGatti=totaleGatti%file
+
+
+console.log(`ci sono ${ Math.round(totaleGatti/file)} file di gatti e ne mancano ${file-restoGatti} per una nuova fila, con un avanzo di ${restoGatti} `)
+
